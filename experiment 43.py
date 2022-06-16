e3e9b3b7f8b495d6a8dfdc5ef782e2f67f100091
ram <ccvy.ram@gmail.com>
2022-06-16 10:43:25 +0530

    insertdata

--- experiment 43.py
+++ experiment 43.py
@@ -8,4 +8,14 @@ def createTable():
     cur.execute('''
 create table emp(employee_id int,first_name varchar2(20),last_name varchar2(30), email text,phone_number int,hire_date text,job_id int,salary int,commision_pct real,manager_id text, department_id text)
 ''')
-createTable()
+
+def insertData(eid,fname,lname, email,pn,hiredata, jid,salary,com,managerid,deptid):
+    cur.execute('''
+    insert into emp (employee_id ,first_name ,last_name , email ,phone_number,hire_date ,job_id ,salary ,commision_pct ,manager_id , department_id ) values (?,?,?,?,?,?,?,?,?,?,?)
+    ''',(eid,fname,lname, email,pn,hiredata, jid,salary,com,managerid,deptid))
+    con.commit()
+    print('data inserted successfully')
+
+
+#createTable()
+
